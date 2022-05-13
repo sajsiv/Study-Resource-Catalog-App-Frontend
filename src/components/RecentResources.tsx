85262fb58db7cbe16ea1e186d0232be541dbbf86
Soso Ogan <soso737@hotmail.co.uk>
2022-05-13 12:11:28 +0100

    added view recent resources functionality

--- src/components/RecentResources.tsx
+++ src/components/RecentResources.tsx
@@ -1,7 +1,52 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import SingleStudyResource from "./SingleStudyResource";
+interface ResourceDataInterface {
+  name: string;
+  author_name: string;
+  url: string;
+  description: string;
+  tags: string;
+  content_type: string;
+  stage: string;
+  original_recommendation: string;
+  recommendation_reasoning: string;
+  userid: number;
+  resourceid: number;
+  creation_date: string;
+}
 export default function RecentResources(): JSX.Element {
+  const [allResources, setAllResources] = useState<ResourceDataInterface[]>([]);
+  useEffect(() => {
+    const fetchResources = async () => {
+      const response = await axios.get("http://localhost:4000/resources");
+      const allResources = await response.data;
+      console.log(allResources);
+      setAllResources(allResources);
+    };
+    fetchResources();
+  }, []);
+
   return (
     <>
-      <h1>Recent Resources</h1>
+      <h1>Recent Banana Resources</h1>
+      {allResources.map((resource) => (
+        <SingleStudyResource
+          resourceName={resource.name}
+          authorName={resource.author_name}
+          resourceType={resource.content_type}
+          creationDate={resource.creation_date}
+          URL={resource.url}
+          description={resource.description}
+          tags={resource.tags}
+          buildPhaseWeek={resource.stage}
+          recommendation={resource.original_recommendation}
+          reasonForRecommendation={resource.recommendation_reasoning}
+          userId={resource.userid}
+          resourceId={resource.resourceid}
+          key={resource.resourceid}
+        />
+      ))}
     </>
   );
 }
